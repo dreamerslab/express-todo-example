2dfff6603a00941f588119db352b8602df052f78
ben <ben@dreamerslab.com>
2012-05-28 21:20:59 +0800

    use /create route for create instead of /

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 var routes = require('./routes');
 
 app.get('/', routes.index);
-app.post('/', routes.create);
+app.post('/create', routes.create);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
