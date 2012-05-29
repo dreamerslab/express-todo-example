b18c81396937bc5e699444fa851852dd40439526
ben <ben@dreamerslab.com>
2012-05-29 10:51:34 +0800

    add edit route

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ var routes = require('./routes');
 app.get('/', routes.index);
 app.post('/create', routes.create);
 app.get('/destroy/:id', routes.destroy);
+app.get('/edit/:id', routes.edit);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
