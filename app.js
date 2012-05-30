f4f31326888dc9c31a430cd5e16e66f13520d802
ben <ben@dreamerslab.com>
2012-05-30 09:13:19 +0800

    change port

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.get('/destroy/:id', routes.destroy);
 app.get('/edit/:id', routes.edit);
 app.post('/update/:id', routes.update);
 
-app.listen(3000, function(){
+app.listen(3001, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
