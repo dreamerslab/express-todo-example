33f49f350daa24526e36b73f755efe6217d31c37
ben <ben@dreamerslab.com>
2013-12-11 10:51:09 -0800

    upgrade to express3

--- app.js
+++ app.js
@@ -3,45 +3,44 @@
  */
 
 var express = require( 'express' );
-
-var app = module.exports = express.createServer();
+var http    = require( 'http' );
+var path    = require( 'path' );
+var engine  = require( 'ejs-locals' )
+var app     = express();
 
 // mongoose setup
 require( './db' );
 
 var routes = require( './routes' );
 
-// Configuration
-app.configure( 'development', function (){
-  app.set( 'views', __dirname + '/views' );
-  app.set( 'view engine', 'ejs' );
-  app.use( express.favicon());
-  app.use( express.static( __dirname + '/public' ));
-  app.use( express.logger());
-  app.use( express.cookieParser());
-  app.use( express.bodyParser());
-  app.use( routes.current_user );
-  app.use( app.router );
-  app.use( express.errorHandler({ dumpExceptions : true, showStack : true }));
-});
+// all environments
+app.set( 'port', process.env.PORT || 3001 );
+app.engine( 'ejs', engine );
+app.set( 'views', path.join( __dirname, 'views' ));
+app.set( 'view engine', 'ejs' );
+app.use( express.favicon());
+app.use( express.logger( 'dev' ));
+app.use( express.cookieParser());
+app.use( express.bodyParser());
+app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.methodOverride());
+app.use( routes.current_user );
+app.use( app.router );
+app.use( express.static( path.join( __dirname, 'public' )));
 
-app.configure( 'production', function (){
-  app.set( 'views', __dirname + '/views' );
-  app.set( 'view engine', 'ejs' );
-  app.use( express.cookieParser());
-  app.use( express.bodyParser());
-  app.use( routes.current_user );
-  app.use( app.router );
+// development only
+if( 'development' == app.get( 'env' )){
   app.use( express.errorHandler());
-});
+}
 
 // Routes
-app.get( '/', routes.index );
-app.post( '/create', routes.create );
-app.get( '/destroy/:id', routes.destroy );
-app.get( '/edit/:id', routes.edit );
-app.post( '/update/:id', routes.update );
+app.get(  '/',            routes.index );
+app.post( '/create',      routes.create );
+app.get(  '/destroy/:id', routes.destroy );
+app.get(  '/edit/:id',    routes.edit );
+app.post( '/update/:id',  routes.update );
 
-app.listen( 3001, '127.0.0.1', function (){
-  console.log( 'Express server listening on port %d in %s mode', app.address().port, app.settings.env );
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
 });
